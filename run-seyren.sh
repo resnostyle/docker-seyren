afe20f278652ae4e749437cb4ed62f189e722f89
Justin Brehm <jhbrehm@gmail.com>
2016-02-09 14:17:56 -0500

    Sourcing in credentials file if it exists

--- run-seyren.sh
+++ run-seyren.sh
@@ -10,6 +10,10 @@ if [ -z ${GRAPHITE_URL} ]
        export GRAPHITE_URL=$1
 fi
 
+set +x
+[ -e /etc/secrets/credentials ] && source /etc/secrets/credentials
+set -x
+
 export MONGO_URL=mongodb://$MONGODB_PORT_27017_TCP_ADDR:$MONGODB_PORT_27017_TCP_PORT/seyren
 
 echo "Graphite URL $GRAPHITE_URL"
